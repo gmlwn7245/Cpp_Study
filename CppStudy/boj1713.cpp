51f251823dd2cf33db865b304de2fc1eaf1a6f54
KimHuiju <gmlwn7245@naver.com>
2022-11-30 22:56:44 +0900

    221130

--- CppStudy/boj1713.cpp
+++ CppStudy/boj1713.cpp
@@ -1,49 +1,85 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <map>
 #include <queue>
 
 using namespace std;
 
 int N, M;
-// 들어온 시점
-map<int, int> time;
-
-// 개수
+int idx, nums;
+vector<int> frame;
 map<int, int> cnt;
-int student[];
+map<int, int> times;
 
-struct Person {
-	int reCnt;
-	int time;
-
-	Person(int reCnt, int time) : reCnt(reCnt), time(time) {};
-
-	bool operator<(const Person p) const {
-		if (this->reCnt == p.reCnt)
-			return this->time < p.time;		// 게시된 지 오래된 순
-		return this->reCnt < p.reCnt;		// 추천 수가 적은 순
-	}
-};
-
-struct cmp {
-	bool operator()(int idx1, int idx2) {
-		if (cnt.find(idx1) == cnt.find(idx2))
-			return time.find(idx1) < time.find(idx2);
-		return cnt.find(idx1) < cnt.find(idx2);
-	}
-};
-
+void findMinIdx();
 
 int main() {
 	cin >> N >> M;
 
-	vector<Person> priorV;
-	vector<int> conV;
 	for (int i = 0; i < M; i++) {
 		int num;
 		cin >> num;
+		
+		// 이미 프레임에 존재
+		if (find(frame.begin(), frame.end(), num) != frame.end()) {
+			cnt[num] += 1;
+			continue;
+		}
 
+		// 프레임이 다 차지 않았을 경우
+		if (frame.size() < N) {
+			frame.push_back(num);
+			cnt[num] = 1;
+			times[num] = i;
+			continue;
+		}
 
+		// 프레임이 꽉 찼을 경우 
+		findMinIdx();
+		
+		cnt.erase(nums);
+		times.erase(nums);
+		frame.erase(frame.begin() + idx);
+
+		frame.push_back(num);
+		cnt[num] = 1;
+		times[num] = i;
+		
 	}
+	
+	sort(frame.begin(), frame.end());
+
+	for (int i = 0; i < frame.size(); i++) {
+		cout << frame[i] << " ";
+	}
+}
+
+// cnt 작은 값 찾기
+void findMinIdx() {
+	int minCnt = INT_MAX;
+	int minTime = INT_MAX;
+	int ansIdx = 0;
+	int ansNum = 0;
+
+	
+	for (int i = 0; i < frame.size(); i++) {
+		cout << "fin"<<i<<"\n";
+		int c = cnt.at(frame[i]);
+		int t = times.at(frame[i]);
+		if (c == minCnt && minTime > t) {
+			minTime = t;
+			ansIdx = i;
+			ansNum = frame[i];
+		}
+		else if (c < minCnt) {
+			minCnt = c;
+			minTime = t;
+			ansIdx = i;
+			ansNum = frame[i];
+		}
+	}
+
+	idx = ansIdx;
+	nums = ansNum;
 }
